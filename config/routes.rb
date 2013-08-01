42db9b1838eac3c9fb141ef3c5405114bb258e21
Matthew Crouch <matthew.a.crouch@gmail.com>
2013-07-31 21:44:50 -0400

    very basic view, just to test for a few days of activity

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 TeamspeakHistory::Application.routes.draw do
+  get "users/index"
+
+  root 'users#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
