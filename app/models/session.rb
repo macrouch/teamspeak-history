29c208d6b2366fdf0e2e92df033d508753cf0894
Matthew Crouch <matthew.a.crouch@gmail.com>
2013-08-08 00:44:02 -0400

    added some unit tests to test out my time conversion
    looks better now, going to try it out live for a day or two and see
    what it looks like and find more test cases

--- app/models/session.rb
+++ app/models/session.rb
@@ -16,59 +16,30 @@ class Session < ActiveRecord::Base
 		doc.css('ul.userlist li').each do |d| # each row in table
 
 			# Find the logout time
-			logout = d.css('div.uhist_list_time').inner_text.downcase
-			logout_time = nil
-			old = false
-			today = DateTime.now(0100) # Now in +0100 (Germany)(Where tsviewer is located)
-			time = logout.split(' ')[1].split(':')
-			if logout.include?('online') || logout.include?('today')
-				logout_time = DateTime.new(today.year, today.month, today.day, time[0].to_i, time[1].to_i, 00, '+0100').in_time_zone
-			elsif logout.include?('yesterday')
-				logout_time = DateTime.new(today.year, today.month, 1.day.ago.day, time[0].to_i, time[1].to_i, 00, '+0100').in_time_zone
-			else # if the session ended before 'yesterday', we don't care about it anymore
-				old = true
-			end
+			logout, logout_time = logout_from_css(d.css('div.uhist_list_time'))
 
 			# if they logged out more than a couple of days ago, don't do anything
-			unless old
+			if logout_time
 
 				# Find the user name
-				# Saw a name like "cool name <-See Me". The '<' character messed everything up.
-				# Just in case we can split on 'Logintime:', and it will pull out only the actual name
-				name = d.css('div.uhist_list_nick_reg strong').first.content.split('Logintime:')[0].strip
-				user = User.find_or_create_by(name: name)
+				user = user_from_css(d.css('div.uhist_list_nick_reg strong').first)
 
 				# Find the channel name
-				channel_name = d.css('div.uhist_list_channel').inner_text.gsub(/\P{ASCII}/, '').strip
-				channel = Channel.find_or_create_by(name: channel_name)
+				channel = channel_from_css(d.css('div.uhist_list_channel'))
 
 				# Find the login time
-				login_ago = d.css('div.uhist_list_nick_reg span').inner_text.split('Logintime: ')[1].split(',')[0]
-				login_days_ago = login_ago.split('D')[0].to_i
-				login = d.css('div.uhist_list_time_connect').inner_text
-				time = login.split(' ')[1].split(':')
-				login_time = (DateTime.new(today.year, today.month, today.day, time[0].to_i, time[1].to_i, 00, '+0100') - login_days_ago.days).in_time_zone
+				login_time = login_from_css(d.css('div.uhist_list_nick_reg span'), 
+																		d.css('div.uhist_list_time_connect'), 
+																		logout_time)
 
 				# Find idle time in seconds "0D 00:41:24" -> 2484
-				idle = d.css('div.uhist_list_nick_reg span').inner_text.split('Idletime: ')[1]
-				# number of days * 86400
-				# number of hours * 3600
-				# number of minutes * 60
-				time = idle.split(' ')[1].split(':')
-				idle = (idle.split('D')[0].to_i * (86400)) + 
-								(time[0].to_i * 3600)	+ 
-								(time[1].to_i * 60)	+ 
-								(time[2].to_i)
+				idle = idle_from_css(d.css('div.uhist_list_nick_reg span'))
 
 				# Find the session, or create a new one
 				previous_session = user.sessions.order("login desc").first
 				session = nil
 				if previous_session
 					# if the previous session is the current session
-					if user.name == "DG| bentbarrel | 2LT"
-						puts "previous_session == #{previous_session.login.strftime('%Y-%m-%d %H:%M:%S')}"
-						puts "login_time ==       #{login_time.strftime('%Y-%m-%d %H:%M:%S')}"
-					end
 					if previous_session.login.strftime('%Y-%m-%d %H:%M:%S') == login_time.strftime('%Y-%m-%d %H:%M:%S')
 						session = previous_session
 					else
@@ -98,8 +69,71 @@ class Session < ActiveRecord::Base
 				# add the channel to the session unless it is already there
 				session.channels << channel unless session.channels.include?(channel)
 
-			end # end unless old
+			end # end unless logout
 		end # end userlist loop
+	end
+
+	def self.user_from_css(css)
+		# Saw a name like "cool name <-See Me". The '<' character messed everything up.
+		# Just in case we can split on 'Logintime:', and it will pull out only the actual name
+		name = css.content.split('Logintime:')[0].strip	
+		User.find_or_create_by(name: name)	
+	end
+
+	def self.channel_from_css(css)
+		channel_name = css.inner_text.gsub(/\P{ASCII}/, '').strip
+		Channel.find_or_create_by(name: channel_name)		
+	end
+
+	def self.login_from_css(css, time_css, logout)
+		logout = logout.in_time_zone(1)
+		today = DateTime.now.in_time_zone(1) # in +0100 (Germany)(Where tsviewer is located)
+		login_time_ago = css.inner_text.split('Logintime: ')[1].split(',')[0]
+		logged_in_time = login_time_ago.split(' ')[1].split(':')
+		# login_days = logout.day - (logout - login_time_ago.split('D')[0].to_i.days).day
+		# login_hours = logged_in_time[0]
+		# login_minutes = logged_in_time[1]
+		login_seconds = (login_time_ago.split('D')[0].to_i * (86400)) + 
+											(logged_in_time[0].to_i * 3600)	+ 
+											(logged_in_time[1].to_i * 60)	+ 
+											(logged_in_time[2].to_i)
+
+		login_days = logout.day - (logout - login_seconds.seconds).day
+		login = time_css.inner_text
+		time = login.split(' ')[1].split(':')
+		((today.day - logout.day).days.ago - login_days.days + (time[0].to_i - today.hour).hours + 
+			(time[1].to_i - today.minute).minutes + (0 - today.second).seconds).in_time_zone	
+	end
+
+	def self.logout_from_css(css)
+		today = DateTime.now.in_time_zone(1) # in +0100 (Germany)(Where tsviewer is located)
+		logout = css.inner_text.downcase
+		time = logout.split(' ')[1].split(':')
+		if logout.include?('online') || logout.include?('today')
+			[logout, (today + (time[0].to_i - today.hour).hours + 
+				(time[1].to_i - today.minute).minutes + (0 - today.second).seconds).in_time_zone]
+		elsif logout.include?('yesterday')
+			[logout, (today - 1.day + (time[0].to_i - today.hour).hours + 
+				(time[1].to_i - today.minute).minutes + (0 - today.second).seconds).in_time_zone]
+		else # if the session ended before 'yesterday', we don't care about it anymore
+			[logout, nil]
+		end		
+	end
+
+	def self.idle_from_css(css)
+		idle = css.inner_text.split('Idletime: ')[1]
+		# number of days * 86400
+		# number of hours * 3600
+		# number of minutes * 60
+		time = idle.split(' ')[1].split(':')
+		idle = (idle.split('D')[0].to_i * (86400)) + 
+						(time[0].to_i * 3600)	+ 
+						(time[1].to_i * 60)	+ 
+						(time[2].to_i)		
+	end
+
+	def self.session_from_css(css)
+
 	end
 
 	protected
--- spec/models/session_spec.rb
+++ spec/models/session_spec.rb
@@ -1,6 +1,23 @@
 require 'spec_helper'
 
 describe Session do
+	def user_css(name)
+		Nokogiri::HTML("<strong>#{name}</strong>")
+	end
+
+	def channel_css(name)
+		Nokogiri::HTML("<div class='uhist_list_channel'><img src='http://static.tsviewer.com/images/teamspeak3/standard/16x16_channel_open.png' alt='' width='10' height='10'> &nbsp;#{name}</div>")
+	end
+
+	def login_css(total_time, time)
+		[Nokogiri::HTML("<span>Logintime: #{total_time}, Idletime: 0D 01:29:05</span>"),
+		Nokogiri::HTML("<div class='uhist_list_time_connect'><strong>Today</strong>, #{time}</div>")]
+	end
+
+	def logout_css(day, time)
+		Nokogiri::HTML("<div class='uhist_list_time'><strong><font color='#c80007'>#{day}</font></strong>, #{time}</div>")
+	end
+
 	it "is valid with valid attributes" do
 		Fabricate(:session).should be_valid
 	end
@@ -8,4 +25,136 @@ describe Session do
 	it { should validate_presence_of :login }
 	it { should validate_presence_of :idle }
 	it { should belong_to :user }
+
+	it "selects user correctly" do
+		user = Session.user_from_css(user_css("User 1"))
+		user.should be_valid
+	end
+
+	it "selects channel correctly" do
+		channel = Session.channel_from_css(channel_css("Channel 1"))
+		channel.should be_valid
+	end
+
+	it "converts times correctly"	do
+		time = DateTime.now.in_time_zone(1)
+		today = time.strftime("%Y-%m-%d")
+		yesterday = (time - 1.days).strftime("%Y-%m-%d")
+		two_days_ago = (time - 2.days).strftime("%Y-%m-%d")
+		three_days_ago = (time - 3.days).strftime("%Y-%m-%d")
+		four_days_ago = (time - 4.days).strftime("%Y-%m-%d")
+
+		logout = (time + (23 - time.hour).hours + (0 - time.minute).minutes + (0 - time.second).seconds).in_time_zone
+		css = login_css("0D 01:00:00", "22:00")
+		login = Session.login_from_css(css[0], css[1], logout)
+		login.to_s.should eq("#{today} 20:00:00 UTC")
+		css = login_css("1D 01:00:00", "22:00")
+		login = Session.login_from_css(css[0], css[1], logout)
+		login.to_s.should eq("#{yesterday} 20:00:00 UTC")
+		css = login_css("2D 01:00:00", "22:00")
+		login = Session.login_from_css(css[0], css[1], logout)
+		login.to_s.should eq("#{two_days_ago} 20:00:00 UTC")
+		css = login_css("3D 01:00:00", "22:00")
+		login = Session.login_from_css(css[0], css[1], logout)
+		login.to_s.should eq("#{three_days_ago} 20:00:00 UTC")
+
+		logout = (time + (2 - time.hour).hours + (0 - time.minute).minutes + (0 - time.second).seconds).in_time_zone
+		css = login_css("0D 01:00:00", "01:00")
+		login = Session.login_from_css(css[0], css[1], logout)
+		login.to_s.should eq("#{yesterday} 23:00:00 UTC")
+		css = login_css("1D 01:00:00", "01:00")
+		login = Session.login_from_css(css[0], css[1], logout)
+		login.to_s.should eq("#{two_days_ago} 23:00:00 UTC")
+		css = login_css("2D 01:00:00", "01:00")
+		login = Session.login_from_css(css[0], css[1], logout)
+		login.to_s.should eq("#{three_days_ago} 23:00:00 UTC")
+		css = login_css("3D 01:00:00", "01:00")
+		login = Session.login_from_css(css[0], css[1], logout)
+		login.to_s.should eq("#{four_days_ago} 23:00:00 UTC")
+
+		logout = (time + (2 - time.hour).hours + (0 - time.minute).minutes + (0 - time.second).seconds).in_time_zone
+		css = login_css("0D 01:00:00", "00:00")
+		login = Session.login_from_css(css[0], css[1], logout)
+		login.to_s.should eq("#{yesterday} 22:00:00 UTC")
+		css = login_css("1D 01:00:00", "00:00")
+		login = Session.login_from_css(css[0], css[1], logout)
+		login.to_s.should eq("#{two_days_ago} 22:00:00 UTC")
+		css = login_css("2D 01:00:00", "00:00")
+		login = Session.login_from_css(css[0], css[1], logout)
+		login.to_s.should eq("#{three_days_ago} 22:00:00 UTC")
+		css = login_css("3D 01:00:00", "00:00")
+		login = Session.login_from_css(css[0], css[1], logout)
+		login.to_s.should eq("#{four_days_ago} 22:00:00 UTC")
+
+		logout = (time + (2 - time.hour).hours + (0 - time.minute).minutes + (0 - time.second).seconds).in_time_zone
+		css = login_css("0D 03:00:00", "23:00")
+		login = Session.login_from_css(css[0], css[1], logout)
+		login.to_s.should eq("#{yesterday} 21:00:00 UTC")
+		css = login_css("1D 03:00:00", "23:00")
+		login = Session.login_from_css(css[0], css[1], logout)
+		login.to_s.should eq("#{two_days_ago} 21:00:00 UTC")
+		css = login_css("2D 03:00:00", "23:00")
+		login = Session.login_from_css(css[0], css[1], logout)
+		login.to_s.should eq("#{three_days_ago} 21:00:00 UTC")
+		css = login_css("3D 03:00:00", "23:00")
+		login = Session.login_from_css(css[0], css[1], logout)
+		login.to_s.should eq("#{four_days_ago} 21:00:00 UTC")
+
+		logout = (time + (3 - time.hour).hours + (0 - time.minute).minutes + (0 - time.second).seconds).in_time_zone
+		css = login_css("0D 01:00:00", "02:00")
+		login = Session.login_from_css(css[0], css[1], logout)
+		login.to_s.should eq("#{today} 00:00:00 UTC")
+		css = login_css("1D 01:00:00", "02:00")
+		login = Session.login_from_css(css[0], css[1], logout)
+		login.to_s.should eq("#{yesterday} 00:00:00 UTC")
+		css = login_css("2D 01:00:00", "02:00")
+		login = Session.login_from_css(css[0], css[1], logout)
+		login.to_s.should eq("#{two_days_ago} 00:00:00 UTC")
+		css = login_css("3D 01:00:00", "02:00")
+		login = Session.login_from_css(css[0], css[1], logout)
+		login.to_s.should eq("#{three_days_ago} 00:00:00 UTC")
+	end
+
+	it "converts logout correctly"	do
+		time = Time.now.in_time_zone(1)
+		today = time.strftime("%Y-%m-%d")
+		tomorrow = (time + 1.day).strftime("%Y-%m-%d")
+		yesterday = (time - 1.days).strftime("%Y-%m-%d")
+		two_days_ago = (time - 2.days).strftime("%Y-%m-%d")
+
+		logout, logout_time = Session.logout_from_css(logout_css("ONLINE","23:00"))
+		logout_time.to_s.should eq("#{today} 21:00:00 UTC")
+		logout, logout_time = Session.logout_from_css(logout_css("Today","23:00"))
+		logout_time.to_s.should eq("#{today} 21:00:00 UTC")
+		logout, logout_time = Session.logout_from_css(logout_css("Yesterday","23:00"))
+		logout_time.to_s.should eq("#{yesterday} 21:00:00 UTC")
+
+		logout, logout_time = Session.logout_from_css(logout_css("ONLINE","00:00"))
+		logout_time.to_s.should eq("#{yesterday} 22:00:00 UTC")
+		logout, logout_time = Session.logout_from_css(logout_css("Today","00:00"))
+		logout_time.to_s.should eq("#{yesterday} 22:00:00 UTC")
+		logout, logout_time = Session.logout_from_css(logout_css("Yesterday","00:00"))
+		logout_time.to_s.should eq("#{two_days_ago} 22:00:00 UTC")
+
+		logout, logout_time = Session.logout_from_css(logout_css("ONLINE","00:30"))
+		logout_time.to_s.should eq("#{yesterday} 22:30:00 UTC")
+		logout, logout_time = Session.logout_from_css(logout_css("Today","00:30"))
+		logout_time.to_s.should eq("#{yesterday} 22:30:00 UTC")
+		logout, logout_time = Session.logout_from_css(logout_css("Yesterday","00:30"))
+		logout_time.to_s.should eq("#{two_days_ago} 22:30:00 UTC")
+
+		logout, logout_time = Session.logout_from_css(logout_css("ONLINE","01:00"))
+		logout_time.to_s.should eq("#{yesterday} 23:00:00 UTC")
+		logout, logout_time = Session.logout_from_css(logout_css("Today","01:00"))
+		logout_time.to_s.should eq("#{yesterday} 23:00:00 UTC")
+		logout, logout_time = Session.logout_from_css(logout_css("Yesterday","01:00"))
+		logout_time.to_s.should eq("#{two_days_ago} 23:00:00 UTC")
+
+		logout, logout_time = Session.logout_from_css(logout_css("ONLINE","02:00"))
+		logout_time.to_s.should eq("#{today} 00:00:00 UTC")
+		logout, logout_time = Session.logout_from_css(logout_css("Today","02:00"))
+		logout_time.to_s.should eq("#{today} 00:00:00 UTC")
+		logout, logout_time = Session.logout_from_css(logout_css("Yesterday","02:00"))
+		logout_time.to_s.should eq("#{yesterday} 00:00:00 UTC")
+	end
 end
